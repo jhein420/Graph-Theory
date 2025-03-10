1a4a744432313b7a3b4c8edb652f9e543ed670b8
Justin <jhein420@protonmail.com>
2025-03-10 16:46:04 +0200

    adjacency matrices

---
+++ graph_theory_implementation_2/mtest_adjList.py
@@ -0,0 +1,27 @@
+#adjacency lists
+#This graph is represented as a collection of linked lists.
+#There is an array of pointer which points to the edges connected to that vertex. 
+
+def add_edge(adj, i, j):
+    adj[i].append(j)
+    adj[j].append(i)  # Undirected
+
+def display_adj_list(adj):
+    for i in range(len(adj)):
+        print(f"{i}: ", end="")
+        for j in adj[i]:
+            print(j, end=" ")
+        print()
+
+# Create a graph with 4 vertices and no edges
+V = 4
+adj = [[] for _ in range(V)]
+
+# Now add edges one by one
+add_edge(adj, 0, 1)
+add_edge(adj, 0, 2)
+add_edge(adj, 1, 2)
+add_edge(adj, 2, 3)
+
+print("Adjacency List Representation:")
+display_adj_list(adj)
